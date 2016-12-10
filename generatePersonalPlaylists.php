82e163fd8c6c244778dfb0ddf55934bbd2f55d64
Huy <njrbailey@outlook.com>
2016-12-10 03:57:37 +0000

    Some query logic

--- generatePersonalPlaylists.php
+++ generatePersonalPlaylists.php
@@ -2,12 +2,50 @@
 	//Database definitions
 	$username = "";
 	$password = "";
-	$database = "";
 	//Connects to database at the same location as this script
-	mysql_connect(localhost, $username, $password);
+	mysql_connect('127.0.0.1', $username, $password);
 
-	function () {
+	function retrieveUserID(firstname, lastname) {
+		try {
+			$handle = new PDO(//TODO DATABASE GOES HERE);
+		} catch (PDOException $e) {
+			die("Error connecting to database: ". $e->getMessage());
+		}
+		$sql = "SELECT userid FROM Users WHERE firstname = :n, lastname = :n;";
+		$query = $handle->prepare($sql);
+		$query->execute(['n' => firstname, 'm' => lastname]);
 
+		//query fails
+		if ($query->execute() === FALSE) {
+			die('Error running query: '.implode($query->errorInfo(), ' '));
+		}
+		//puts result into assoc array
+		$results = $query->fetchAll();
+
+		//TODO -------- what to do if results are empty? die() at the end
+		return $results;
+	}
+
+	function isStruggling(userid) {
+		try {
+			$handle = new PDO(//TODO DATABASE GOES HERE);
+		} catch (PDOException $e) {
+			die("Error connecting to database: ". $e->getMessage());
+		}
+		$sql = "SELECT exercise_id FROM Exercise_log WHERE user_id = :n;";
+		$query = $handle->prepare($sql);
+		$query->execute(['n' => userid);
+
+		//query fails
+		if ($query->execute() === FALSE) {
+			die('Error running query: '.implode($query->errorInfo(), ' '));
+		}
+		//puts result into assoc array
+		$results = $query->fetchAll();
+
+		//TODO -------- what to do if results are empty? die() at the end
+		return $results;
+		//Will be a list of exercise_id that the student is flagged as struggling with
 	}
 
 ?>
