4603f3d7fd888c2a3fbaf528c2c9c94fc7f8aef7
Huy <njrbailey@outlook.com>
2016-12-10 07:51:55 +0000

    html

---
+++ edulutionApp.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+</head>
+<body>
+	<section>
+		<form method='GET' action='generatePersonalPlaylist.php'>
+			Firstname: <input type='text' value=firstname>
+			Lastname: <input type='text' value=lastname>
+			<input type='submit' value='submit'>
+		</form>
+	</section>
+	<section id=dataTable>
+	</section>
+</body>
+</html>
