5ee0381406289d504a301a59f575a95a51c987b9
Huy <njrbailey@outlook.com>
2016-12-10 03:18:23 +0000

    message

---
+++ generatePersonalPlaylists.php
@@ -0,0 +1,13 @@
+<?php
+	//Database definitions
+	$username = "";
+	$password = "";
+	$database = "";
+	//Connects to database at the same location as this script
+	mysql_connect(localhost, $username, $password);
+
+	function () {
+
+	}
+
+?>
